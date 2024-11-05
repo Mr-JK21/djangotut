3c44e48adf926c54f4fe15fa3393cd4467f4adb1
JK108912 <jhilkambad108912@gmail.com>
2024-11-05 23:39:37 +0530

    initial commit for myproject

---
+++ myproject/myproject/views.py
@@ -0,0 +1,16 @@
+# from django.http import HttpResponse
+from django.shortcuts import render
+
+
+def homepage(request):
+    return render(request, "home.html")
+
+
+#    return HttpResponse("Hello World!")
+
+
+def about(request):
+    return render(request, "about.html")
+
+
+#    return HttpResponse("This is the about page")
